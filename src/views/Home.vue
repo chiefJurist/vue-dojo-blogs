6e2334c095a53a846bf5247d6e3f412460d55d79
chiefJurist <anthonynnanna004@gmail.com>
2023-08-21 00:22:45 +0100

    computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,24 @@
 <template>
     <div class="home">
         <h1>Home</h1>
-        <h2>Refs</h2>
-        <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-        <button @click="updateNinjaOne">Update ninja one</button>
-        <h2>Reactive</h2>
-        <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-        <button @click="updateNinjaTwo">Update ninja two</button>
+        <input type="text" v-model="search">
+        <p>search term - {{ search }}</p>
+        <div v-for="name in matchingNames" :key="name">{{ name }}</div>
     </div>
 </template>
 
 <script>
-    import { reactive, ref } from 'vue'
+    import { computed, ref } from 'vue'
     export default {
         name: 'Home',
         setup(){
-            //You can also use objects as refs
-            const ninjaOne = ref({name: 'Mario', age: 30})
-            const ninjaTwo = reactive({name: 'Luigi', age: 35})
+            const search = ref('')
+            const names = ref(['Mario', 'Yoshi', 'Luigi', 'Tony', 'Bowser', 'Koopa', 'Peach'])
 
-            const updateNinjaOne = ()=> {
-                ninjaOne.value.age = 40
-            }
-
-            //You do not need to use the ".value" when using reactive
-            //Note that you can not use primitive data types with reactive. That is their limitation
-            //You can only use objects with reactive
-            const updateNinjaTwo = ()=> {
-                ninjaTwo.age = 45
-            }
-
-            return{ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
+            const matchingNames = computed(() => {
+                return names.value.filter((name)=> name.includes(search.value))
+            })
+            return { names,search,matchingNames }
         }
     }
 </script>
