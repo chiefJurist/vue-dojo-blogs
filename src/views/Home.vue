841777cdb7a8c944394e534173471f70d15ab852
chiefJurist <anthonynnanna004@gmail.com>
2023-08-20 23:32:05 +0100

    refs vs reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,36 @@
 <template>
     <div class="home">
         <h1>Home</h1>
-        <p>My name is {{ name }} and my age is {{ age }}</p>
-        <button @click="handleClick">Click me</button><br>
-        <button @click="age++">Click to add 1 to age</button><br>
-        <input type="text" v-model="name">
+        <h2>Refs</h2>
+        <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+        <button @click="updateNinjaOne">Update ninja one</button>
+        <h2>Reactive</h2>
+        <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+        <button @click="updateNinjaTwo">Update ninja two</button>
     </div>
 </template>
 
 <script>
-    //importing the ref from vue
-    import { ref } from 'vue'
+    import { reactive, ref } from 'vue'
     export default {
         name: 'Home',
         setup(){
-            const name = ref("Anthony")
-            const age = ref(20)
+            //You can also use objects as refs
+            const ninjaOne = ref({name: 'Mario', age: 30})
+            const ninjaTwo = reactive({name: 'Luigi', age: 35})
 
-            const handleClick = ()=> {
-                name.value = "Luigi"
-                age.value = 35
+            const updateNinjaOne = ()=> {
+                ninjaOne.value.age = 40
             }
 
-            return{name, age, handleClick}
+            //You do not need to use the ".value" when using reactive
+            //Note that you can not use primitive data types with reactive. That is their limitation
+            //You can only use objects with reactive
+            const updateNinjaTwo = ()=> {
+                ninjaTwo.age = 45
+            }
+
+            return{ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
         }
     }
 </script>
