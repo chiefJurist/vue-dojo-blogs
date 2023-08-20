8016e2fb5b003723b1a4c4b61540f7e227866db1
chiefJurist <anthonynnanna004@gmail.com>
2023-08-20 22:18:14 +0100

    composition api basics

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="home">
-
-  </div>
+    <div class="home">
+        home
+        <p>My name is {{ name }} and my age is {{ age }}</p>
+        <button @click="handleClick">Click me</button>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-export default {
-  name: 'Home'
-}
+    export default {
+        name: 'Home',
+        setup(){
+            console.log('setup')
+
+            //These are not reactive values
+            //That means they don't update in the template when changed
+            let name = "Anthony"
+            let age = 20
+
+            const handleClick = ()=> {
+                console.log('You clicked me')
+            }
+
+            //Any data you want out side, you just have to return it
+            //return{name: name, age: age}
+
+            //Note that as the name of the property and the value is the same as seen above, you can just remove the prop name
+            return{name, age, handleClick}
+        }
+    }
 </script>
