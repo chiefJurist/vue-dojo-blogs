f78637273b24d9b8be5c1b9a8790ff149d6c3e8b
chiefJurist <anthonynnanna004@gmail.com>
2023-08-20 22:42:12 +0100

    template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="home">
         home
-        <p>My name is {{ name }} and my age is {{ age }}</p>
+        <p ref="para">
+            My name is {{ name }} and my age is {{ age }}
+        </p>
         <button @click="handleClick">Click me</button>
     </div>
 </template>
 
 <script>
+    //importing the ref from vue
+    import { ref } from 'vue'
     export default {
         name: 'Home',
         setup(){
             console.log('setup')
 
-            //These are not reactive values
-            //That means they don't update in the template when changed
+            //Using template refs in composition api
+            //Don't use the refs before you return them
+            const para = ref(null)
             let name = "Anthony"
             let age = 20
 
             const handleClick = ()=> {
-                console.log('You clicked me')
+                console.log(para, para.value)
+                //Work with the value and not just the name of the ref
+                para.value.classList.add('test')
+                para.value.textContent = "Hello, Ninjas"
             }
 
-            //Any data you want out side, you just have to return it
-            //return{name: name, age: age}
-
-            //Note that as the name of the property and the value is the same as seen above, you can just remove the prop name
-            return{name, age, handleClick}
+            return{name, age, handleClick, para}
         }
     }
 </script>
