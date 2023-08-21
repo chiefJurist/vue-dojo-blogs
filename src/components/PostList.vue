e91b39a627e85ff7bd41267fea37561433f90259
chiefJurist <anthonynnanna004@gmail.com>
2023-08-21 09:09:53 +0100

    Just used computed properties in setup() to output a snippet

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import SinglePost from "./SinglePost.vue";
+    export default {
+        props: ['posts'],
+        components: { SinglePost },
+        setup(props){
+            //If you want to use props with the setup(). Just remve the console.log
+            console.log(props.posts)
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,23 @@
 <template>
     <div class="home">
         <h1>Home</h1>
-        <input type="text" v-model="search">
-        <p>search term - {{ search }}</p>
-        <div v-for="name in matchingNames" :key="name">{{ name }}</div><br>
-        <button @click="handleClick">Stop Watch</button>
+        <PostList :posts="posts" />
     </div>
 </template>
 
 <script>
-    import { computed, ref, watch, watchEffect } from 'vue'
+    import PostList from '../components/PostList.vue'
+    import {  ref } from 'vue'
     export default {
         name: 'Home',
+        components: { PostList },
         setup(){
-            const search = ref('')
-            const names = ref(['Mario', 'Yoshi', 'Luigi', 'Tony', 'Bowser', 'Koopa', 'Peach'])
+            const posts = ref([
+                {title: 'Welcome to the blog', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, autem exercitationem. Voluptate veniam repellendus quasi ullam similique dicta est quam culpa natus dolor pariatur earum consequatur officiis laudantium possimus, deserunt qui quisquam sapiente at voluptas expedita repudiandae, nisi commodi in! Quidem, aspernatur aliquam nulla, rerum, placeat dolor neque saepe alias in facilis deleniti! Dignissimos reiciendis autem quos molestiae facere, soluta culpa velit itaque, eligendi rerum nobis tempora, tempore officiis placeat voluptatem nesciunt! Fugiat quidem maiores, assumenda dolorum placeat est aliquam minus beatae voluptates corporis maxime, consequatur error. Commodi vitae alias nam veniam enim, molestiae dolorem esse, corrupti id ex nihil.', id: 1},
+                {title: 'Top 5 CSS tips', body: 'Lorem ipsum', id: 2}
+            ])
 
-            const stopWatch = watch(search, ()=>{
-                console.log("Watch fuction ran")
-            })
-
-            //This runs at loading
-            //Also you don't have to explicitly state the values you want to watch as you do in "watch()"
-            //It watches any value placed inside of it
-            const stopEffect = watchEffect(()=>{
-                console.log("WatchEffect function ran", search.value)
-            })
-
-            const matchingNames = computed(() => {
-                return names.value.filter((name)=> name.includes(search.value))
-            })
-
-            //for stopping the watch and the WatchEffect
-            const handleClick = () => {
-                stopWatch()
-                stopEffect()
-            }
-
-            return { names,search,matchingNames, handleClick }
+            return { posts }
         }
     }
 </script>
